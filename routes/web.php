23d03118fbe60fd895e54b1483ba1ff838f0f624
zaelly <zazabarbosa2@gmail.com>
2025-04-10 16:35:33 -0300

    route corrigido

--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');*/
+Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('authentication-failed', function () {
     $errors = [];
